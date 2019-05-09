c7de88b15f6a9e43369b78cd3db054a65e507fd7
NicolasAsis <nico.asis15@gmail.com>
2019-05-09 15:56:09 -0700

    Added to JSON File

--- data.js
+++ data.js
@@ -3,9 +3,34 @@ var data = {
         {
             step:"1",
             title:"Find the Stem",
-            animation:
-            desc:"blah blah"
+            animation:"",
+            desc:"The middle hard part of the mango is the stem. Face the mango up and cut along the stem on both sides."
         },
-
+        {
+            step:"2",
+            title:"Slice into the mango",
+            animation:"",
+            desc:"With one of the halves, slice vertical lines, piercing through the mango, but not through the skin. Repeat this for the other half."
+        },
+        {
+            step:"3",
+            title:"Flatten and Cut",
+            animation:"",
+            desc:"Flatten the mango out with your hand. Put your knife horizontal and cut along the skin, making mango slices."
+        },
+        {
+            step:"4",
+            title:"Put mango on plate",
+            animation:"",
+            desc:"After you cut the slices out of the mango, you can now place it onto the plate and enjoy."
+        }
+    ],
+    "Mango Cubes":[
+      {
+          step:"1",
+          title:"Find the Stem",
+          animation:"",
+          desc:"The middle hard part of the mango is the stem. Face the mango up and cut along the stem on both sides."
+      },
     ]
 }
