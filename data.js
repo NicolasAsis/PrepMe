bb1b3b07dff98c9b27bb77c23cbba143e7f48859
NicolasAsis <nico.asis15@gmail.com>
2019-05-10 00:23:09 -0700

    Added data to JSON file

--- data.js
+++ data.js
@@ -32,5 +32,172 @@ var data = {
           animation:"",
           desc:"The middle hard part of the mango is the stem. Face the mango up and cut along the stem on both sides."
       },
-    ]
+      {
+          step:"2",
+          title:"Slice into the mango(vertical)",
+          animation:"",
+          desc:"With one of the halves, slice vertical lines, piercing through the mango, but not through the skin. Repeat this for the other half."
+      },
+      {
+          step:"3",
+          title:"Slice into the mango(horizontal)",
+          animation:"",
+          desc:"Use the knife to make horizontal cuts in the two mango cheeks. The cuts should not go through the skin."
+      },
+       {
+          step:"4",
+          title:"Put the mango inside out",
+          animation:"",
+          desc:"Invert the mango half so that the cut segments are sticking out like a hedgehog."
+      },
+       {
+          step:"5",
+          title:"Trim the cubes off",
+          animation:"",
+          desc:"Now, you can trim the cubes of mango off."
+      },
+        {
+          step:"6",
+          title:"Put mango on plate",
+          animation:"",
+          desc:"After you cut the slices out of the mango, you can now place it onto the plate and enjoy."
+      },
+
+    ],
+    "Avacado Cubes":[
+      {
+          step:"1",
+          title:"Cut into two halves",
+          animation:"",
+          desc:"Gently cut around the avocado to separate into two halves. Make sure to cut around the seed in the middle, not through it."
+      },
+      {
+          step:"2",
+          title:"Take away one Half",
+          animation:"",
+          desc:"Take away one of the slices that you cut."
+      },
+      {
+          step:"3",
+          title:"Remove the seed",
+          animation:"",
+          desc:"Remove the seed by slipping a spoon between the seed and fruit."
+      },
+      {
+          step:"4",
+          title:"Linear cuts on the flesh",
+          animation:"",
+          desc:"Use a knife to slice through the avocado flesh without cutting through the outer skin. "
+      },
+      {
+          step:"5",
+          title:"Linear cuts opposite way",
+          animation:"",
+          desc:"Do the same as previous but this time cut through the skin the opposite"
+      },
+      {
+          step:"6",
+          title:"Serve the Slices",
+          animation:"",
+          desc:"Afterwards use a spoon and scoop out cubes out of the outer skin. Put all the slices onto a plate and serve!"
+      }
+    ],
+    "Avacado Slice":[
+      {
+          step:"1",
+          title:"Cut into two halves",
+          animation:"",
+          desc:"Gently cut around the avocado to separate into two halves. Make sure that you don't cut the seed inside."
+      },
+      {
+          step:"2",
+          title:"Take away one Half",
+          animation:"",
+          desc:"Take away one of the slices that you cut."
+      },
+      {
+          step:"3",
+          title:"Remove the seed",
+          animation:"",
+          desc:"Remove the seed by slipping a spoon between the seed and fruit."
+      },
+      {
+          step:"4",
+          title:"Linear cuts on the flesh",
+          animation:"",
+          desc:"Use a knife to slice through the avocado flesh without cutting through the outer skin. "
+      },
+      {
+          step:"5",
+          title:"Serve the Slices",
+          animation:"",
+          desc:"Afterwards use a spoon and scoop out sliced out of the outer skin. Put all the slices onto a plate and serve!"
+      }
+      ],
+      "Pomegranate":[
+      {
+          step:"1",
+          title:"Remove the flower top of the fruit",
+          animation:"",
+          desc:"Use a paring knife to remove the flower from the top of the pomegranate."
+      },
+      {
+          step:"2",
+          title:"Score the Sides",
+          animation:"",
+          desc:"Score along these wider rib portions of the pomegranate, take the paring knife and start at your previous cut at the flower end of the pomegranate, and cut the skin along the ridge down toward the bottom (stem) end of the fruit."
+      },
+      {
+          step:"3",
+          title:"Crack open",
+          animation:"",
+          desc:"Place your thumbs inside the open end, with the pads of your thumbs pressing against opposing segments of the pomegranate. Pull apart gently, but firmly so that the pomegranate cracks open along the lines that you've cut in the skin.  The internal segments will separate along with it."
+      },
+      {
+          step:"4",
+          title:"Remove seeds",
+          animation:"",
+          desc:"Put the seeds onto a plate and it's your done!"
+      }
+      ],
+      "Pineapple Cube":[
+        {
+          step:"1",
+          title:"Cut off top and bottom",
+          animation:"",
+          desc:"Using the knife, cut off about half an inch of the top and bottom of the pineapple."
+        },
+        {
+          step:"2",
+          title:"Trim the sides",
+          animation:"",
+          desc:"Stand the pineapple upright, and cut downward strips, following the contours of the pineapple.  This will skin the pineapple."
+        },
+        {
+          step:"3",
+          title:"Remove the outer skin and  eyes",
+          animation:"",
+          desc:"To remove the outer skin and the eyes, continue to cut long strips all the way around the pineapple. Use a small paring knife to remove any remaining eyes(the holes)."
+        },
+        {
+          step:"4",
+          title:"Remove the core",
+          animation:"",
+          desc:"Slice the pineapple in half from top to bottom and then  slice off the corner of each quarter."
+        },
+        {
+          step:"5",
+          title:"Make it small and  make a diced",
+          animation:"",
+          desc:"To make a long piece for smaller, cut each quarter again into 2 or 3 lengthwise strips. Make bite size wedges by cutting across the strips."
+        },
+        {
+          step:"6",
+          title:"Enjoy",
+          animation:"",
+          desc:""
+        },
+      ]
+
+
 }
